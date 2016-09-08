4fb509549dcd5b44f503f0fbf7b824f29ffdfd60
shreyas <bhandare.shreyas@gmail.com>
2016-09-08 16:11:48 -0400

    first tryout version

--- newSystemScript.sh
+++ newSystemScript.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
+sudo -s
+
 #update and upgrade respository before installing
 apt-get update && sudo apt-get upgrade
 apt-get dist-upgrade
 
 # text editors
-apt-get install vim emacs gedit
+apt-get install -y vim emacs gedit
 
 # python 2.7.11
-apt-get install python -y
+apt-get install -y python 
 
 # idle
-apt-get install idle -y
+apt-get install -y idle
 
 # git
-apt-get install git
+apt-get install -y git
 
 # gcc g++
-apt-get install g++ gcc
+apt-get install -y g++ gcc
 
 # gdb (C++ debugger)
-apt-get install gdb
+apt-get install -y gdb
 
 # sublime
-apt-get install sublime-text
+apt-get install -y sublime-text
 
 # oracle virtual box
-apt-get install virtualbox-5.1 
+apt-get install -y virtualbox-5.1 
 
 # GNU octave
-apt-get install octave
+apt-get install -y octave
 
 #java jdk jre
-apt-get install default-jre
-apt-get install default-jdk
+apt-get install -y default-jre
+apt-get install -y default-jdk
 add-apt-repository ppa:webupd8team/java
 apt-get update 
-apt-get install oracle-java8-installer
+apt-get install -y oracle-java8-installer
 update-alternatives --display java
 echo 'JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> /etc/environment
 
@@ -48,35 +50,36 @@ chmod 777 studio.sh
 ./studio.sh
 
 #chrome
-apt-get install google-chrome-stable
+apt-get install -y google-chrome-stable
 
 #vlc
-apt-get install vlc
+apt-get install -y vlc
 
 #----------------Devops Tools---------------------#
 
 # packer
 wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
-apt-get install unzip
+apt-get install -y unzip
 unzip packer_0.10.1_linux_amd64.zip -d /usr/local/
 export PATH=$PATH:/usr/local/
 
 # vagrant
-apt-get install vagrant 
+apt-get install -y vagrant 
 
 #ansible 
-apt-get install ansible
+apt-get install -y ansible
 
 #docker (everything as sudo)
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 rm /etc/apt/sources.list.d/docker.list
 touch /etc/apt/sources.list.d/docker.list
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get purge lxc-docker
-apt-get install docker-engine
+apt-get install -y docker-engine
 docker --version
 
-
+echo 'End of the installtion script'
+# End of the installtion script
